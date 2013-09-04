5b0d5bc159084bd939df080ac0c28101a526523b
Shusui MOYATANI <syusui.s@gmail.com>
2013-09-04 23:35:01 +0900

    タスク一覧を追加。UIに画像を追加。KancolletTableにタイマの追加・削除の機能追加。

--- kancollet/kancollet.js
+++ kancollet/kancollet.js
@@ -2,7 +2,11 @@ function KancolletTimer(type,num,name){
 	this.type    = type;
 	this.num     = num;
 	this.name    = name;
+	this.time    = null;
 	this.element = null;
+	// private
+	this.endtime = null;
+	this.timer   = null;
 }
 
 KancolletTimer.prototype.appendElement = function(){
@@ -35,11 +39,12 @@ KancolletTimer.prototype.appendElement = function(){
 			break;
 		}
 	}
-	th.appendChild(td);
+	this.element = th.appendChild(td);
 }
 
-KancolletTimer.prototype.settingTimer = function(elem){
-	id = elem.getAttribute('id');
+KancolletTimer.prototype.settingTimer = function(time){
+	this.time = time;
+	this.element.
 }
 
 function KancolletTable(){
@@ -51,7 +56,7 @@ KancolletTable.prototype.appendElement = function(){
 	var table = document.createElement('table');
 	table.setAttribute('id','kancollet-timers-table');
 
-	types = {expedition:'遠征',dock:'入渠',arsenal:'建造'}
+	var types = {expedition:'遠征',dock:'入渠',arsenal:'建造'};
 	for(key in types){
 		var tr = table.insertRow(-1);
 		var th = document.createElement('th');
@@ -61,6 +66,30 @@ KancolletTable.prototype.appendElement = function(){
 		tr.appendChild(th);
 	}
 	this.element = document.getElementById('kancollet-timers-form').appendChild(table);
+}
+
+KancolletTable.prototype.addTimer = function(type,num,name){
+	if(!this.timers[type+'-'+num.toString()]){
+		var key = type+'-'+num.toString();
+		var timer = new KancolletTimer(type,num,name);
+		timer.appendElement();
+		this.timers[key] = timer;
+		return true;
+	}else{
+		return false;
+	}
+}
+
+KancolletTable.prototype.removeTimer = function(type,num){
+	var key = type+'-'+num.toString();
+	var timer = this.timers[type+'-'+num.toString()]
+	if(timer){
+		var parentnode = timer.element.parentNode;
+		parentnode.removeChild(timer.element);
+		delete this.timers[key];
+	}else{
+		return false;
+	}
 }
 
 function KancolletPreset(){
@@ -73,12 +102,75 @@ function removeKancollet(){
 
 function createKancollet(){
 	var kancollet = document.createElement('div');
-	kancollet.setAttribute('id','kancollet');
+	kancollet.id = 'kancollet';
 	
-	var kancollet_timers = document.createElement('form');
-	kancollet_timers.setAttribute('id','kancollet-timers-form');
+	var kancollet_timers_form = document.createElement('form');
+	kancollet_timers_form.id = 'kancollet-timers-form';
 
-	var kancollet_setting_form = document.createElement('form');
-	kancollet_timers.setAttribute('id','kancollet-timers-form');
+	var kancollet_timersetting_form = document.createElement('form');
+	kancollet_timersetting_form.id = 'kancollet-timersetting-form';
+	
+	var kancollet_timersetting_time = document.createElement('input');
+	kancollet_timersetting_time.id = 'kancollet-setting-time';
+	kancollet_timersetting_time.type = 'time';
+	kancollet_timersetting_time.step = '1';
 
+	var kancollet_timersetting_start = document.createElement('input');
+	kancollet_timersetting_start.type = 'button';
+	kancollet_timersetting_start.value = '開始';
+
+	var kancollet_timersetting_stop = document.createElement('input');
+	kancollet_timersetting_stop.type = 'button';
+	kancollet_timersetting_stop.value = '停止');
+
+	var kancollet_timersetting_softwarename = document.createElement('img');
+	kancollet_timersetting_softwarename.id = 'kancollet-timersetting-softwarename';
+	kancollet_timersetting_softwarename.src = './kancollet/img/kancollet.png';
+	kancollet_timersetting_softwarename.alt = 'Kancollet';
+	kancollet_timersetting_softwarename.width  = '62';
+	kancollet_timersetting_softwarename.height = '11';
+
+	var kancollet_timersetting_setting = document.createElement('a');
+	kancollet_timersetting_setting.setAttribute('href','#');
+	kancollet_timersetting_setting.innerText = '設定';
+
+	var kancollet_timersetting_close = document.createElement('a');
+	kancollet_timersetting_close.href = 'javascript:removeKancollet();');
+	var kancollet_timersetting_closeimg = document.createElement('img');
+	kancollet_timersetting_closeimg.id = 'kancollet-timersetting-closeimg'
+	kancollet_timersetting_closeimg.src = './kancollet/img/close_button.png';
+	kancollet_timersetting_closeimg.alt = '閉じる';
+	kancollet_timersetting_closeimg.width  = '16';
+	kancollet_timersetting_closeimg.height = '16';
+	kancollet_timersetting_close.appendChild(kancollet_timersetting_closeimg);
+
+	var kancollet_timersetting_cleardiv = document.createElement('div');
+	kancollet_timersetting_cleardiv.class = 'clear';
+	kancollet_timersetting_cleardiv.appendChild(document.createElement('hr'));
+
+	kancollet_timersetting_form.appendChild(kancollet_timersetting_time);
+	kancollet_timersetting_form.appendChild(kancollet_timersetting_start);
+	kancollet_timersetting_form.appendChild(kancollet_timersetting_stop);
+	kancollet_timersetting_form.appendChild(document.createElement('br'));
+	kancollet_timersetting_form.appendChild(kancollet_timersetting_softwarename);
+	kancollet_timersetting_form.appendChild(kancollet_timersetting_setting);
+	kancollet_timersetting_form.appendChild(kancollet_timersetting_close);
+	kancollet_timersetting_form.appendChild(kancollet_timersetting_cleardiv);
+
+	kancollet.appendChild(kancollet_timers_form);
+	kancollet.appendChild(kancollet_timersetting_form);
+
+	document.body.appendChild(kancollet);
+	kancollet_timers_table = new KancolletTable(); // keep this global to debug
+	kancollet_timers_table.appendElement();
+
+	kancollet_timers_table.addTimer('expedition',1,'第二艦隊');
+	kancollet_timers_table.addTimer('expedition',2,'第三艦隊');
+	kancollet_timers_table.addTimer('expedition',3,'第四艦隊');
+	
+	kancollet_timers_table.addTimer('dock',1,'ドック1');
+	kancollet_timers_table.addTimer('dock',2,'ドック2');
+
+	kancollet_timers_table.addTimer('arsenal',1,'ドック1');
+	kancollet_timers_table.addTimer('arsenal',2,'ドック2');
 }
---
+++ kancollet_task.markdown
@@ -0,0 +1,7 @@
+# MAIN
+[ ] KancolletTimerにより、タイマーを設定できるようにする
+[ ] KancolletTimerにstart,stop,configメソッドを追加
+
+# DONE
+[x] KancolletTableでタイマーの追加・削除などを行えるようにインタフェースを作る。
+[x] JavaScriptによるGUIの実装をする。createKancolletの作成。
