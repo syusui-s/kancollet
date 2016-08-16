b917188c4c05b9de42aa70bfc60d0a668b6bd13b
Shusui MOYATANI <syusui.s@gmail.com>
2016-08-16 13:38:13 +0900

    開発版の使用方法を記述した

--- kancollet/bookmarklet.js
+++ kancollet/bookmarklet.js
@@ -1,5 +1,7 @@
-// ブックマークレットをあなたのブックマークに加えたいのであれば、
-// このスクリプトの5行目をコピーして、「:javascript」をその前に追加してください。
-// そしてそれを『新しいブックマーク』のURL欄にペーストしてください。
-// 名前は、自由につけてください。
-(function(){if(document.getElementById('kancollet_script')){Kancollet.create();}else{var e=document.createElement('script');e.setAttribute('language','javascript');e.id='kancollet_script';e.setAttribute('src','http://syusui-s.github.io/kancollet/kancollet/kancollet.js');e.setAttribute('charset', 'UTF-8');document.head.appendChild(e);}}());
+// Kancolletをあなたのブックマークに加えたいのであれば、
+// このスクリプトの5行目をコピーして、『新しいブックマーク』のURL欄にペーストしてください。
+// ブックマーク名は、自由につけてください。
+javascript:(function(){if(document.getElementById('kancollet_script')){Kancollet.create();}else{var e=document.createElement('script');e.setAttribute('language','javascript');e.id='kancollet_script';e.setAttribute('src','https://raw.githubusercontent.com/syusui-s/kancollet/master/kancollet/kancollet.js');e.setAttribute('charset', 'UTF-8');document.head.appendChild(e);}}());
+
+// 最新の開発版を利用したい場合は、8行目をコピーして、通常の方法と同様にブックマークに追加してください。
+javascript:(function(){if(document.getElementById('kancollet_script')){Kancollet.create();}else{var e=document.createElement('script');e.setAttribute('language','javascript');e.id='kancollet_script';e.setAttribute('src','https://raw.githubusercontent.com/syusui-s/kancollet/develop/kancollet/kancollet.js');e.setAttribute('charset', 'UTF-8');document.head.appendChild(e);}}());
