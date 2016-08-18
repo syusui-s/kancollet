5b7ff0b233961f6063279ccb532ee9b4cfab9bcf
Shusui MOYATANI <syusui.s@gmail.com>
2016-08-18 17:09:57 +0900

    互換性のためのスクリプトを最新版を読ませるように変更

--- kancollet/kancollet.js
+++ kancollet/kancollet.js
@@ -1,7 +1,13 @@
-(function() {
-	window.alert("KancolletのURLが変更されました。お手数ながら、ブックマークの変更をお願いします。");
-	var res = window.confirm("OKを押すと、公式ページに遷移します。よろしいですか？");
-	if (res) {
-		location.href = "http://syusui-s.github.io/kancollet/";
+/* 互換性維持のための多段Javascript読み込み */
+(function(){
+	if(document.getElementById('kancollet_script_0_12_1')){
+		Kancollet.create();
+	} else {
+		var e = document.createElement('script');
+		e.setAttribute('language','javascript');
+		e.id='kancollet_script_0_12_1';
+		e.setAttribute('src','http://syusui-s.github.io/kancollet/kancollet-master/kancollet/kancollet.js');
+		e.setAttribute('charset', 'UTF-8');
+		document.head.appendChild(e);
 	}
-})();
+}());
